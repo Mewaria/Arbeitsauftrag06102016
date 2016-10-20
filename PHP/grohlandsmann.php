a1bd8034c14fa28efe509eb3f2693dccbf188418
Mewaria <mewaria@hotmail.de>
2016-10-20 12:11:58 +0200

    upload ordner

--- PHP/grohlandsmann.php
+++ PHP/grohlandsmann.php
@@ -1,10 +1,11 @@
 <?php 
-
+//Debug
+/*
 echo $_GET['fst'];
 echo $_GET['sth'];
 echo $_GET['ast'];
 echo $_GET['baujahr'];
-
+*/
 $handle =  fopen("kosten.csv","w");
 $data = '"Fläsche je Stockwerk in qm";"Stockwerk in m";"Anzahl Stockwerke";"Baujahr"' . "\n";
 $data .='"'. $_GET['fst'] .
