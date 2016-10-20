f7a576cf6d91b761b39be6fa78b1dd40164f77bc
Mewaria <mewaria@hotmail.de>
2016-10-20 11:44:48 +0200

    change php file

---
+++ PHP/grohlandsmann.php
@@ -0,0 +1,22 @@
+<?php 
+
+echo $_GET['fst'];
+echo $_GET['sth'];
+echo $_GET['ast'];
+echo $_GET['baujahr'];
+
+$handle =  fopen("kosten.csv","w");
+$data = '"Fläsche je Stockwerk in qm";"Stockwerk in m";"Anzahl Stockwerke";"Baujahr"' . "\n";
+$data .='"'. $_GET['fst'] .
+ '";"'.
+ $_GET['sth'] .
+ '";"'.
+ $_GET['ast'] .
+ '";"'.
+ $_GET['baujahr'] .
+ '"'.
+ "\n";
+fwrite($handle,$data);
+fclose($handle);
+
+?>
